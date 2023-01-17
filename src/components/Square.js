81061a90f9abbd2819e2d60d757ec4c78df7fa82
louisql <102247814+louisql@users.noreply.github.com>
2023-01-17 10:29:27 -0500

    lifting state up to the Board

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,10 +1,6 @@
 import React from "react";
 
 const Square = props => {
-    const clickHandler = () => {
-        console.log('clicked baby')
-    }
-
 
     return(
         <button className="square" onClick={clickHandler}>{props.value}</button>
