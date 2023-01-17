fd9eca5cf495d23ce1614c090e8256af76dc2bea
louisql <102247814+louisql@users.noreply.github.com>
2023-01-17 09:44:20 -0500

    Fixing bugs from starting project and implementing Square component

--- src/App.js
+++ src/App.js
@@ -1,3 +1,27 @@
-export default function Square() {
-  return <button className="square">X</button>;
-}
+import React from "react";
+import { Fragment } from "react";
+import Square from "./components/Square";
+
+const Board = () => {
+  return (
+    <React.Fragment>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default Board
---
+++ src/components/Square.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Square = props => {
+    return(
+        <button className="square">1</button>
+    )
+}
+
+export default Square
